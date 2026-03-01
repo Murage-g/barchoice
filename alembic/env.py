b52daadf1d088a5d3fcc6b63d9a7c496fcf75cc5
Murage_g <muragegeorgem@gmail.com>
2026-03-01 13:17:06 +0300

    fixed all tables

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from alembic import context
 from sqlalchemy import create_engine, pool
 
--- alembic/versions/db8399b9051a_create_purchase_offers_table.py
+++ alembic/versions/db8399b9051a_create_purchase_offers_table.py
@@ -19,8 +19,8 @@ def upgrade() -> None:
     op.create_table(
         'purchase_offers',
         sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('purchase_id', sa.Integer, sa.ForeignKey('purchases.id', ondelete='CASCADE'), nullable=False),
-        sa.Column('product_id', sa.Integer, sa.ForeignKey('products.id', ondelete='CASCADE'), nullable=False),
+        sa.Column('purchase_id', sa.Integer, sa.ForeignKey('purchase.id', ondelete='CASCADE'), nullable=False),
+        sa.Column('product_id', sa.Integer, sa.ForeignKey('product.id', ondelete='CASCADE'), nullable=False),
         sa.Column('quantity', sa.Integer, nullable=False),
         sa.Column('offer_date', sa.DateTime, nullable=False, server_default=sa.func.now()),
         sa.Column('created_by', sa.String(length=120), nullable=True),
